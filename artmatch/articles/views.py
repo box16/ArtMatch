7864fe445c7a4d5f7d3c37d139048d41ed0b33c8
box16 <inoinobox16@gmail.com>
2020-12-29 10:16:04 +0900

    indexのview templateの紐づけ

--- artmatch/articles/views.py
+++ artmatch/articles/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
-
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the articles index.")
+    context = {"main_message" : "Hello There"}
+    return render(request,"articles/index.html",context)
