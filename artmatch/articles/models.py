94f6f808fc6a5623cb965d56b6f591c13114438b
box16 <inoinobox16@gmail.com>
2020-12-29 10:57:23 +0900

    articlesモデルの作成

--- artmatch/articles/models.py
+++ artmatch/articles/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Articles(models.Model):
+    title = models.TextField()
+    url = models.TextField()
+    body = models.TextField()
+
+    def __str__(self):
+        return f"{self.title}"
