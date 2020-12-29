3b70905e24b4e23100d661e766dc43d01a1d3b40
box16 <inoinobox16@gmail.com>
2020-12-29 11:11:27 +0900

    Index汎用ビューの実装

--- artmatch/articles/views.py
+++ artmatch/articles/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Articles
 
-def index(request):
-    context = {"main_message" : "Hello There"}
-    return render(request,"articles/index.html",context)
+class IndexView(generic.ListView):
+    template_name = "articles/index.html"
+    context_object_name = "pick_up_articles"
+
+    def get_queryset(self):
+        """とりあえず、頭5個をピックアップとしている"""
+        return Articles.objects.all()[:5]
