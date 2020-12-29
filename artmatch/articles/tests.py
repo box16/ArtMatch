34ac5b4afd3669c532bd8a1209923c6e8ba2cbee
box16 <inoinobox16@gmail.com>
2020-12-29 11:21:09 +0900

    IndexViewのテスト作成

--- artmatch/articles/tests.py
+++ artmatch/articles/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from .models import Articles
+
+def create_article(title="title",url="url",body="body"):
+    return Articles.objects.create(title=title,url=url,body=body)
+
+class IndexViewTests(TestCase):
+    def test_no_articles(self):
+        response = self.client.get(reverse('articles:index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No articles are available.")
+        self.assertQuerysetEqual(response.context['pick_up_articles'], [])
+    
+    def test_has_articles(self):
+        a = create_article()
+        response = self.client.get(reverse('articles:index'))
+        self.assertQuerysetEqual(
+            response.context['pick_up_articles'],
+            ['<Articles: title>']
+        )
--- artmatch/articles/urls.py
+++ artmatch/articles/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name="articles"
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
 ]
