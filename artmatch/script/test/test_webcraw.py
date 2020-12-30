0a763c955f2f285f2c8cd9fbb439d592c89d1ce9
box16 <inoinobox16@gmail.com>
2020-12-30 10:12:31 +0900

    collect_urls作成

--- artmatch/script/test/test_webcraw.py
+++ artmatch/script/test/test_webcraw.py
@@ -1,17 +1,35 @@
 import unittest
 from ..webcraw import Crawler
+import re
 
 class TestWebcraw(unittest.TestCase):
     def setUp(self):
         self.crawler = Crawler()
+        self.website = {"name": "PaleolithicMan",
+                        "domain": "https://yuchrszk.blogspot.com/",
+                        "title_tag": "[class='post-title single-title emfont']",
+                        "body_tag": "[class='post-single-body post-body']",
+                        "link_collector": re.compile("^(?=https://yuchrszk.blogspot.com/..../.+?)(?!.*archive)(?!.*label).*$"),
+                        "link_creater": lambda domestic_url: domestic_url,
+                        }
 
     def test_get_page_normal_url(self):
-        bs = self.crawler.get_page("https://docs.python.org/ja/3/library/unittest.html")
+        bs = self.crawler.get_page(self.website["domain"])
         self.assertIsNotNone(bs)
     
     def test_get_page_abnormal_url(self):
         bs = self.crawler.get_page("")
         self.assertIsNone(bs)
+
+    def test_collect_urls_normal_url(self):
+        urls = self.crawler.collect_urls(self.website["domain"],
+                                       self.website["link_collector"])
+        self.assertGreater(len(urls), 0)
+
+    def test_collect_urls_abnormal_url(self):
+        urls = self.crawler.collect_urls("",
+                                       self.website["link_collector"])
+        self.assertEqual(len(urls), 0)
 
 if __name__ == "__main__":
     unittest.main()
--- artmatch/script/webcraw.py
+++ artmatch/script/webcraw.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 class Crawler:
     def __init__(self):
         pass
@@ -13,8 +12,13 @@ class Crawler:
             return None
         return BeautifulSoup(req.text, "html.parser")
     
-    def _collect_urls(self,base_url,link_collector):
+    def collect_urls(self,base_url,link_collector):
         bs = self.get_page(base_url)
+        if not bs:
+            return []
+        result_urls = []
         for link in bs.find_all("a", href=link_collector):
             if not link.attrs["href"]:
                 continue
+            result_urls.append(link.attrs["href"])
+        return list(set(result_urls))
