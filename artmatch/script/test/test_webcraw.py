4e1fe99d0807f85d5dd6cbca9622a851a3aecbd8
box16 <inoinobox16@gmail.com>
2020-12-30 10:26:13 +0900

    safe_get作成

--- artmatch/script/test/test_webcraw.py
+++ artmatch/script/test/test_webcraw.py
@@ -14,22 +14,48 @@ class TestWebcraw(unittest.TestCase):
                         }
 
     def test_get_page_normal_url(self):
-        bs = self.crawler.get_page(self.website["domain"])
-        self.assertIsNotNone(bs)
-    
+        bs_object = self.crawler.get_page(self.website["domain"])
+        self.assertIsNotNone(bs_object)
+
     def test_get_page_abnormal_url(self):
-        bs = self.crawler.get_page("")
-        self.assertIsNone(bs)
+        bs_object = self.crawler.get_page("")
+        self.assertIsNone(bs_object)
 
     def test_collect_urls_normal_url(self):
-        urls = self.crawler.collect_urls(self.website["domain"],
-                                       self.website["link_collector"])
+        bs_object = self.crawler.get_page(self.website["domain"])
+        urls = self.crawler.collect_urls(bs_object,
+                                         self.website["link_collector"])
         self.assertGreater(len(urls), 0)
 
     def test_collect_urls_abnormal_url(self):
-        urls = self.crawler.collect_urls("",
-                                       self.website["link_collector"])
+        bs_object = self.crawler.get_page("")
+        urls = self.crawler.collect_urls(bs_object,
+                                         self.website["link_collector"])
         self.assertEqual(len(urls), 0)
+
+    def test_safe_get_normal_url_title(self):
+        bs_object = self.crawler.get_page("https://yuchrszk.blogspot.com/2020/12/20208.html")
+        elems = self.crawler.safe_get(bs_object,
+                                     self.website["title_tag"])
+        self.assertGreater(len(elems), 0)
+
+    def test_safe_get_abnormal_url_title(self):
+        bs_object = self.crawler.get_page("")
+        elems = self.crawler.safe_get(bs_object,
+                                     self.website["title_tag"])
+        self.assertEqual(len(elems), 0)
+    
+    def test_safe_get_normal_url_body(self):
+        bs_object = self.crawler.get_page("https://yuchrszk.blogspot.com/2020/12/20208.html")
+        elems = self.crawler.safe_get(bs_object,
+                                        self.website["body_tag"])
+        self.assertGreater(len(elems), 0)
+
+    def test_safe_get_abnormal_url_body(self):
+        bs_object = self.crawler.get_page("")
+        elems = self.crawler.safe_get(bs_object,
+                                     self.website["body_tag"])
+        self.assertEqual(len(elems), 0)
 
 if __name__ == "__main__":
     unittest.main()
--- artmatch/script/webcraw.py
+++ artmatch/script/webcraw.py
@@ -12,13 +12,21 @@ class Crawler:
             return None
         return BeautifulSoup(req.text, "html.parser")
     
-    def collect_urls(self,base_url,link_collector):
-        bs = self.get_page(base_url)
-        if not bs:
+    def collect_urls(self,bs_object,link_collector):
+        if not bs_object:
             return []
         result_urls = []
-        for link in bs.find_all("a", href=link_collector):
+        for link in bs_object.find_all("a", href=link_collector):
             if not link.attrs["href"]:
                 continue
             result_urls.append(link.attrs["href"])
         return list(set(result_urls))
+
+    def safe_get(self, bs_object, css_selector):
+        if not bs_object:
+            return []
+        selected_elems = bs_object.select(css_selector)
+        if (selected_elems is not None) and (len(selected_elems) > 0):
+            return '\n'.join([elem.get_text() for elem in selected_elems])
+        else:
+            return []
