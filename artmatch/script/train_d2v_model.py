76dc1f8cfe53c55061031f619a7dc74e49ee60cb
box16 <inoinobox16@gmail.com>
2021-01-02 09:36:43 +0900

    一度しか出現しない単語の削除

--- artmatch/script/train_d2v_model.py
+++ artmatch/script/train_d2v_model.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+from pprint import pprint
 from db_access import DBAccess
 from nlp import NLP
 
@@ -5,8 +7,18 @@ if __name__ == "__main__":
     db_access = DBAccess()
     nlp = NLP()
 
+    # 分かち書き
     bodys = db_access.slect_article_pick_body()
     corpus = []
     for body in bodys:
         corpus.append(nlp.extract_legal_nouns_verbs(body[0]))
-    print(corpus[1:4])
+    
+    # 一度しか出現しない単語の削除
+    frequency = defaultdict(int)
+    for document in corpus:
+        for token in document:
+            frequency[token] += 1
+    corpus = [
+        [token for token in document if frequency[token] > 1]
+        for document in corpus
+    ]
