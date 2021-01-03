b2ef49ad63df8e04d2de91a2caa002d43a030986
box16 <inoinobox16@gmail.com>
2021-01-03 09:25:16 +0900

    類似記事の表示

--- artmatch/articles/views.py
+++ artmatch/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Articles
+from .script import d2v
 
 class IndexView(generic.ListView):
     template_name = "articles/index.html"
@@ -13,6 +14,13 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Articles
     template_name = 'articles/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        similar_articles_id = d2v.D2V().find_similer_articles(self.kwargs['pk'])
+        similar_articles = Articles.objects.in_bulk(similar_articles_id)
+        context['similar_articles'] = similar_articles
+        return context
 
     def get_queryset(self):
          return Articles.objects.all()
