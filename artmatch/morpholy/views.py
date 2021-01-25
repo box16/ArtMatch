2e50a4cef9bdd8243b9e1d590c9f6fa95bdbd9e9
box16 <inoinobox16@gmail.com>
2021-01-25 13:38:13 +0900

    format

--- artmatch/morpholy/views.py
+++ artmatch/morpholy/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views.generic.edit import FormView
 from .forms import MorpholyForm
 
+
 class IndexView(FormView):
     template_name = 'morpholy/index.html'
     form_class = MorpholyForm
